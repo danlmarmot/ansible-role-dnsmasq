a9278e829cf4a92fe9ea372b9732aba55a09013a
dmckean <danlmarmot@gmail.com>
2016-09-30 21:08:15 -0700

    Working checkin

---
+++ examples/install_role_requirements.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# dnsmasq has no role requirements, but if it did they'd be installed like this
+
+#ansible-galaxy install -r role_requirements.yml -p role_dependencies --force
